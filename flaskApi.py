fed0f5c09aa49d1eb4ede8454fe918a801376599
ALICE SINDAYIGAYA <alicesindayigaya@gmail.com>
2018-03-30 10:29:06 +0200

    Update flaskApi.py

--- flaskApi.py
+++ flaskApi.py
@@ -1,51 +1,121 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar 30 10:04:49 2018
-
-@author: to125348
-"""
-
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 29 14:28:30 2018
-
-@author: to125348
-"""
-
 import csv
 import json
 
-from cStringIO import StringIO
-from flask import Flask, render_template, request, make_response
+from flask import Flask, jsonify, abort
+from flask import make_response, request
+from flask import url_for
+from flask_httpauth import HTTPBasicAuth
 
-app =Flask(__name__)
-app.debug = True
 
-def csv_json(data):
-    reader = csv.DictReader
-    reader = csv.DictReader(data)
-    out =json.dumps([ row for row in reader ])
-    print "JSON parsed!"
-    return out
-    print "JSON saved"
+auth = HTTPBasicAuth()
+
     
 
-@app.route('/csv2json', methods=['POST'])
-def convert():
-    f = request.files['data_file']
-    if not f:
-        return "No file"
-    file_contents =StringIO(f.stream.read())
-    result = csv_json(file_contents)
-    response = make_response(result)
-    response.headers["Content-Disposition"] = "attachment; filename=Converted.json"
-    return response
+app= Flask(__name__)
+
+def convert_csv():
     
-@app.route('/')
-def main () :
-    render_template
-    return render_template('convert.html')
+tasks=[
+    {
+        'id':1,
+        'title':u'Buy groceries',
+        'description':u'HP,Asus,Toshiba,Acer',
+        'done':False
+    },
+    {
+        'id':2,
+        'title':u'Learn Python',
+        'description':u'Need to find a good Python tutoriel on the web',
+        'done':False
+            }
+            
+]
+
+# Authentification
+
+@auth.get_password
+def get_password(username):
+    if username == 'epg':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}),403)
+
+
+#Function to generate a public URI
+
+def make_public_task(task):
+    new_task= {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+@auth.login_required
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len (task) == 0:
+        abort(404)
+    return jsonify({'task': [make_public_task(task) for task[0] in tasks]})
+
+@app.errorhandler(404)
+def not_founder(error):
+    return make_response(jsonify({'error': 'Not found'}),404)
+
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+@auth.login_required
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task={
+            'id': tasks[-1]['id'] +1,
+            'title':request.json['title'],
+            'description':request.json.get('description', ""),
+            'done':False
+   }
+
+    tasks.append(task)
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]}),201
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+@auth.login_required
+def update_task(task_id):
+    task= [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(404)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description']= request.json.get('description',task[0]['title'])
+    task[0]['done']= request.json.get('done', task[0]['done'])
+    return jsonify({'task':make_public_task(task[0])})
+
+@app.route('/todo/api/v1.0/tasks/<int:task_id>)', methods=['DELETE'])
+@auth.login_required
+def delete_task(task_id):
+    task=[task for task in tasks if task ['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    task.remove(task[0])
+    return jsonify({'result': True})
+
 
 if __name__=='__main__':
-    app.run()
+    app.run(host = '0.0.0.0', debug =True)
