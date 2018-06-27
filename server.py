46f6802e03bb9b5e8d0a92ce1ddbf12220a4b501
ALICE SINDAYIGAYA <alicesindayigaya@gmail.com>
2018-06-27 16:33:57 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,25 @@
-from flask import Flask, request,jsonify
+#==============================================================================
+# SPPMS WEB SERVICE - WEB SERVER START
+#==============================================================================
+#import system library
+import os
 
+#import flask library
+from flask import Flask, request,jsonify
+from flask import render_template
+
+#Flask application start 
 app = Flask(__name__)
 
-# Make the header for client response
+#Convert path windows in linux real path 
+def getPath(path):
+    server = '\\10.122.80.194'
+    realPath = path.replace(server,'/projects')
+    new_path = realPath.replace('\\','/')
+    file_path =os.path.dirname(new_path)
+    return file_path
+    
+# Define the header for client response
 @app.after_request 
 def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -11,41 +28,36 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
+
 #import libpfm
 import pypfm as pfm
 
-
-#Get value parameter and  launch sppms
+#Get value parameter and  launch sppms by libpfm
 @app.route ("/sppms/api/v1.0", methods= ['GET','POST'])
 def getconfig():
-    
+
     if request.method ==  "POST":
-        
+
         #Get the form value
         form = request.form
         altitude = float(form['altitude'])
         disa = float(form['disa'])
         mach = float(form['mach'])
-        file_path = str(form['file_path'])
-        file_path = "/projects/moteur_ap/A350/RR/BASE/txwb97_1000_ifpa"
+        file_path = getPath(str(form['file_path']))
         regime = str(form['regime'])
         install = str(form['install'])
+        humidite = float(form['humidite'])
+        libpath = str(form['pathLibrary'])
+#        alt_piste = float(form['alt_piste'])
+#        temps_flex=(form['temps_flex'])
+#        typar = str(form['typar'])
+#        vapar = float(form['vapar'])
 
-        print altitude, disa, mach, install, regime, file_path
-        #file_path = getfile(request.data ('file_ part'))
-        #altitude = request.json('altitude')
-        #mach = request.json('mach')
-        #delta = request.json('delta')
-        #humidite = request.json('humidite')
-        #alt_piste = request.json('alt-piste')
-        #temps_flex = request.json ('temps-flex')
-        #typar = request.json('typar')
-        #vapar = request.json('vapa')
-        #install=request.json('install')
+        print altitude, disa, mach, install, regime, file_path,humidite,libpath
         
         #Configure logging
         pfm.Logging.setLevel(pfm.Logging.Debug)
-        
+       
         #Create a test data object
         td = pfm.TestData()
         td.setNumberOfSamples(1)
@@ -53,7 +65,7 @@ def getconfig():
         td.addParameter("XM", "", [mach])
         td.addParameter("DTAMB", "K", [disa])
         td.setContext("SPPMS_IN")
-        
+       
         #Create SPPMS model
         model = pfm.Sppms()
         model.setInputTestData(td)
@@ -68,21 +80,39 @@ def getconfig():
         model.setEngine("TRENTXWB")
         model.setBlPath(file_path)
         model.run()
-        
+     
         #Read output test data
         reader = pfm.createTestDataReader("orma")
         reader.setInputFile("OUT/ENGINE_1_AC/BIN_HOMOLOG_BDM")
         out = reader.read()
         print out, out.countSamples(), out.countParameters()
-        
+      
         #Format json to be sent back
         dct = {}
         for param in out.listParameters():
             dct[param] = out.getParameterValueAtSample(param, 0)
         return jsonify(dct)
-    return "success"
-  
+    return "Success"
+    
+#Homologate library url
+@app.route("/homologate", methods= ['GET','POST'])
+def homologate():
+    return render_template('homologate.html')
+
+#Other library url
+@app.route("/others", methods= ['GET','POST'])
+def others():
+    return render_template('others_lib.html')
+
+#Result and output url 
+@app.route("/result", methods= ['GET','POST'])
+def result():
+    return render_template('result.html')
+
+#main function
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8125, debug=True)
+
+
 
 
